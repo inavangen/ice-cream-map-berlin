05ecf44dccc77622b3a5e5a4918e0235e9cc6456
inavangen <ina.vangen@gmail.com>
2026-04-10 14:42:46 +0200

    try to make custom icons

--- src/components/IceCream.vue
+++ src/components/IceCream.vue
@@ -11,8 +11,10 @@ import L from "leaflet"
 // POI icon paths 
 delete (L.Icon.Default.prototype as any)._getIconUrl
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  //iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  //iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: '../assets/marker2x.png',
+  iconUrl: '../assets/marker2x.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 })
 
